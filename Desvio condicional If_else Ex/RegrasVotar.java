d6cf836e9290690bdb0e40c9ba54568b2b85f7f9
Anderson Junior <anderson.araujo0812@gmail.com>
2026-09-14 11:03:15 -0300

    Add files via upload

--- Desvio condicional If_else Ex/RegrasVotar.java
+++ Desvio condicional If_else Ex/RegrasVotar.java
@@ -1,4 +1,6 @@
-/*
+ 
+ 
+ /*
 Desenvolver um algoritmo para definir se uma pessoa está apta 	a votar no Brasil.
 Pesquise: Quais são as regras para se votar no Brasil?
 Identifique quais os dados de entrada necessários para resolver o problema.
@@ -16,13 +18,30 @@ public class RegrasVotar {
         System.out.println("Preencha os dados para verificar se está apto para votar.");
 
 
-        System.out.println("Você está com seu documento de identidade?");
-        String documento = entrada.nextLine().toLowerCase();
 
-        if (documento.matches("(?i)nao|n|não")){
-            System.out.println("você não pode votar sem o seu documento");
+        System.out.println("Você é brasileiro?(S/N)");
+        String nacionalidade = entrada.nextLine().toLowerCase();
+
+        if (nacionalidade.equals("n")){
+            System.out.println("Você não pode votar sendo estrangeiro");
             System.exit(0);
         }
+
+        System.out.println("Você está com seu documento de identidade?(S/N)");
+        String documento = entrada.nextLine().toLowerCase();
+
+        if (documento.equals("n")){
+            System.out.println("Você não pode votar sem o seu documento");
+            System.exit(0);
+        }
+
+        System.out.println("Você está com seu título de eleitor regular(S/N)?");
+            String titulo = entrada.nextLine().toLowerCase();
+        if (titulo.equals("n")){
+            System.out.println("Você não pode votar sem regular seu título de eleitor");
+            System.exit(0);
+        }
+        
         
         System.out.println("Digite sua idade: ");
         int idade = entrada.nextInt();
